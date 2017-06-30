7d491b68dc0064596b1ab655ee4279862e5a14da
Vasilyeva <nastyavm89@gmail.com>
2017-06-30 19:20:35 +0300

    changing

--- homeTask1/src/TypeConvertingTask1/TypeConverting.java
+++ homeTask1/src/TypeConvertingTask1/TypeConverting.java
@@ -1,18 +1,20 @@
+package TypeConvertingTask1;
+
 /**
  * Created by Anastasiya on 28-Jun-17.
  */
 public class TypeConverting {
     public static void main(String[] args) {
-        ByteConverting();
-        ShortConverting();
-        CharConverting();
-        IntConverting();
-        LongConverting();
-        FloatConverting();
-        DoubleConverting();
+        byteConverting();
+        shortConverting();
+        charConverting();
+        intConverting();
+        longConverting();
+        floatConverting();
+        doubleConverting();
     }
 
-    public static void ByteConverting() {
+    public static void byteConverting() {
         byte a = 8;
         System.out.println("Byte converting, value: " + a);
 
@@ -31,15 +33,15 @@ public class TypeConverting {
         double f = a;
         System.out.println("Byte to double = " + f);
 
-        char h = (char)a;
+        char h = (char) a;
         System.out.println("Byte to char = " + h);
     }
 
-    public static void ShortConverting() {
+    public static void shortConverting() {
         short a = 32;
         System.out.println("Short converting, value: " + a);
 
-        byte b = (byte)a;
+        byte b = (byte) a;
         System.out.println("Short to byte = " + b);
 
         int c = a;
@@ -54,15 +56,15 @@ public class TypeConverting {
         double f = a;
         System.out.println("Short to double = " + f);
 
-        char h = (char)a;
+        char h = (char) a;
         System.out.println("Short to char = " + h);
-
     }
-    public static void CharConverting() {
+
+    public static void charConverting() {
         char a = 'z';
         System.out.println("Char converting, value: " + a);
 
-        byte b = (byte)a;
+        byte b = (byte) a;
         System.out.println("Char to byte = " + b);
 
         int c = a;
@@ -79,17 +81,16 @@ public class TypeConverting {
 
         short h = (short) a;
         System.out.println("Char to short = " + h);
-
-
     }
-    public static void IntConverting() {
+
+    public static void intConverting() {
         int a = 101;
         System.out.println("Int converting, value: " + a);
 
-        byte b = (byte)a;
+        byte b = (byte) a;
         System.out.println("Int to byte = " + b);
 
-        short c = (short)a;
+        short c = (short) a;
         System.out.println("Int to short = " + c);
 
         long d = a;
@@ -101,21 +102,21 @@ public class TypeConverting {
         double f = a;
         System.out.println("Int to double = " + f);
 
-        char h = (char)a;
+        char h = (char) a;
         System.out.println("Int to char = " + h);
-
     }
-    public static void LongConverting() {
+
+    public static void longConverting() {
         long a = 1021;
         System.out.println("Long converting, value: " + a);
 
-        byte b = (byte)a;
+        byte b = (byte) a;
         System.out.println("Long to byte = " + b);
 
-        short c = (short)a;
+        short c = (short) a;
         System.out.println("Long to short = " + c);
 
-        int d = (int)a;
+        int d = (int) a;
         System.out.println("Long to long = " + d);
 
         float e = a;
@@ -124,55 +125,53 @@ public class TypeConverting {
         double f = a;
         System.out.println("Long to double = " + f);
 
-        char h = (char)a;
+        char h = (char) a;
         System.out.println("Long to char = " + h);
-
     }
-    public static void FloatConverting() {
+
+    public static void floatConverting() {
         float a = 3.5f;
         System.out.println("Float converting, value: " + a);
 
-        byte b = (byte)a;
+        byte b = (byte) a;
         System.out.println("Float to byte = " + b);
 
-        short c = (short)a;
+        short c = (short) a;
         System.out.println("Float to short = " + c);
 
-        int d = (int)a;
+        int d = (int) a;
         System.out.println("Float to long = " + d);
 
-        long e = (long)a;
+        long e = (long) a;
         System.out.println("Float to long = " + e);
 
         double f = a;
         System.out.println("Float to double = " + f);
 
-        char h = (char)a;
+        char h = (char) a;
         System.out.println("Float to char = " + h);
-
     }
-    public static void DoubleConverting() {
+
+    public static void doubleConverting() {
         double a = 96.5;
         System.out.println("Double converting, value: " + a);
 
-        byte b = (byte)a;
+        byte b = (byte) a;
         System.out.println("Double to byte = " + b);
 
-        short c = (short)a;
+        short c = (short) a;
         System.out.println("Double to short = " + c);
 
-        int d = (int)a;
+        int d = (int) a;
         System.out.println("Double to long = " + d);
 
-        long e = (long)a;
+        long e = (long) a;
         System.out.println("Double to long = " + e);
 
-        float f = (float)a;
+        float f = (float) a;
         System.out.println("Double to float = " + f);
 
-        char h = (char)a;
+        char h = (char) a;
         System.out.println("Double to char = " + h);
-
     }
-
 }
