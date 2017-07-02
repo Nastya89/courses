83f49b4d79f5a4efe303f53950102e90b7cd6ea0
Vasilyeva <nastyavm89@gmail.com>
2017-07-02 22:38:27 +0300

    task3

--- homeTask1/src/integer/verification/task3/IntegerVerification.java
+++ homeTask1/src/integer/verification/task3/IntegerVerification.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 public class IntegerVerification {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int i;
         System.out.print("Введите целое число: ");
