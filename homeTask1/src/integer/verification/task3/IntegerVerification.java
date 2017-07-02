2e38503947d77c6eb1dea50a4d7fdd45802e1319
Vasilyeva <nastyavm89@gmail.com>
2017-07-02 22:28:28 +0300

    task3

---
+++ homeTask1/src/integer/verification/task3/IntegerVerification.java
@@ -0,0 +1,26 @@
+package integer.verification.task3;
+
+import java.util.Scanner;
+
+/**
+ * Created by Anastasiya on 02-Jul-17.
+ */
+public class IntegerVerification {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int i;
+        System.out.print("Введите целое число: ");
+        if (sc.hasNextInt()) {
+            i = sc.nextInt();
+            if (i % 2 == 0) {
+                System.out.println("Вы ввели число " + i + "." + " Это четное число");
+            } else {
+                System.out.println("Вы ввели число " + i + "." + " Это нечетное число");
+            }
+        } else {
+            System.out.println("Вы ввели не целое число");
+        }
+    }
+}
