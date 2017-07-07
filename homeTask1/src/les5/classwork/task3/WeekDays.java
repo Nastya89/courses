f550b29ee01844b650c342f3304f936106018027
Vasilyeva <nastyavm89@gmail.com>
2017-07-07 20:49:39 +0300

    dz

--- homeTask1/src/les5/classwork/task3/WeekDays.java
+++ homeTask1/src/les5/classwork/task3/WeekDays.java
@@ -10,17 +10,15 @@ public class WeekDays {
             tempValue = Double.parseDouble(str);
             if (tempValue == 1) {
                 System.out.println("Понедельник");
-            } else if ((tempValue == 2)) {
+            } else if (tempValue == 2) {
                 System.out.println("Вторник");
-            } else if ((tempValue == 3)) {
+            } else if (tempValue == 3) {
                 System.out.println("Среда");
-            } else if ((tempValue == 4)) {
+            } else if (tempValue == 4) {
                 System.out.println("Четверг");
-            } else if ((tempValue == 5)) {
+            } else if (tempValue == 5) {
                 System.out.println("Пятница");
-            } else if ((tempValue == 6)) {
-                System.out.println("Выходной");
-            } else if ((tempValue == 7)) {
+            } else if (tempValue == 6 || tempValue == 7) {
                 System.out.println("Выходной");
             } else {
                 System.out.println("Неверное число");
